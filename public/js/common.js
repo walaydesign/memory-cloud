b2783ee3d156a8c40642249ae04db66846d0fa6c
youzhen <yl301114@gmail.com>
2025-12-16 01:17:23 +0000

    連結

--- public/js/common.js
+++ public/js/common.js
@@ -1,11 +1,3 @@
-AOS.init({ startEvent: "load" });
-window.addEventListener("load", AOS.refresh);
-
-// go to top
-$(".btn-top").click(function () {
-  $("html, body").animate({ scrollTop: 0 }, 300);
-});
-
 // menu
 $(".header__menu").click(function () {
   $(this).toggleClass("active");
@@ -30,63 +22,6 @@ $(".product-num button").click(function () {
     num++;
   }
   $(this).parents(".product-num").find(".product-num_val").text(num);
-});
-
-// swiper
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 4,
-  spaceBetween: 30,
-  slidesPerGroup: 6, //一次移動幾個
-  centeredSlides: true, //對準中心
-  loop: true, //無限輪迴
-  direction: "vertical",
-  observeParents: true,
-  observer: true,
-  allowTouchMove: false,
-  thumbs: {
-    swiper: "swiper名稱",
-  },
-  simulateTouch: false, //不能點選
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    type: "progressbar", //型態:進度條
-  },
-  scrollbar: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
-  keyboard: {
-    enable: true, //可用鍵盤控制
-  },
-  effect: "fade",
-  speed: 1000,
-  fadeEffect: {
-    crossFade: true,
-  },
-  allowTouchMove: false,
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    576: {
-      slidesPerView: 2,
-    },
-    768: {
-      slidesPerView: 3,
-    },
-    1024: {
-      slidesPerView: 4,
-    },
-  },
 });
 
 $(".switch").click(function () {
